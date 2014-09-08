335ac5f59809fd092cf43f88a4f85b165012018f
vfa-phuongnh <vfa-phuongnh@gmail.com>
2014-09-08 22:23:25 +0700

    structure application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Time2learn::Application.config.secret_token = 'c023639e2f1f3f56689d51ed85bfa9532b2536d8e095cfcfef100374b13a503a2d439ed00afb45f4e80794c3fd465ca8c713c9455a9380024eace2da8f7b28a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Time2learn::Application.config.session_store :cookie_store, key: '_time2learn_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Time2learn::Application.config.session_store :active_record_store
